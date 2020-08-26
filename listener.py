fe87ab946520bdcb758baf62e04c1bec083c0850
dyslexda <lexda44+github@gmail.com>
2020-08-26 19:16:35 +0000

    Version 0.1

--- listener.py
+++ listener.py
@@ -36,11 +36,9 @@ class SubredditReport:
         return(round(statistics.mean(karma),1))
 
     def first_post(self):
-#        sorted_comments = sorted(self.comments, key=lambda comment: comment.created_utc)
         return(self.comments[-1])
 
     def most_recent_post(self):
-#        sorted_comments = sorted(self.comments, key=lambda comment: comment.created_utc)
         return(self.comments[0])
 
     def posts_in_timeperiod(self,period):
@@ -105,33 +103,15 @@ def comment_level(comment):
     else:
         return(False)
 
-def msg_reply():
-    pass
-
-async def listener():
-    target_sub = 'wisconsin'
-    r = reddit_read()
-    for comment in r.subreddit(target_sub).stream.comments():
-        if comment_level(comment):
-            subs = await auth_report(comment.author)
-            try:
-                day_old = subs[target_sub].posts_older_than('day')
-                week_old = subs[target_sub].posts_older_than('week')
-                if (day_old == 0):
-                    print("Day Old: ", comment.is_root, comment.author.name, comment.permalink)
-                elif (week_old == 0):
-                    print("Week Old: ", comment.is_root, comment.author.name, comment.permalink)
-            except:
-                print(comment.permalink)
-#        print(comment.body)
-
-async def get_refresh():
-    r = reddit_connect()
-#    print(r.auth.scopes())
-#    state = str(random.randint(0, 65000))
-#    url = r.auth.url(["*"], state, "permanent")
-#    print(url)
-#    print(r.auth.authorize('0avQEiNqrNaX2Pi7wb-t7KcAD14'))
+def msg_reply(period):
+    if period == 'new':
+        com_history = 'this is your first post here'
+    elif period == 'day':
+        com_history = 'you joined our community within the last day'
+    elif period == 'week':
+        com_history = 'you joined our community within the last week'
+    msg = (f"Hello, and thank you for partiipating in /r/wisconsin! It appears {com_history}. Please make sure you review our [community guidelines](https://www.reddit.com/r/wisconsin/about/rules/) and [recent rule updates regarding increased outside activity](https://old.reddit.com/r/wisconsin/comments/iglpc4/welcome_new_users_from_other_websites_and/).")
+    return(msg)
 
 async def auth_report(author):
     r_read = reddit_read()
@@ -143,6 +123,41 @@ async def auth_report(author):
 #        creation_time = datetime.fromtimestamp(comment.created_utc).astimezone(central_timezone)
         subs[comment.subreddit.display_name].add_comment(comment)
     return(subs)
+
+async def listener():
+    target_sub = 'wisconsin'
+    r_read = reddit_read()
+    r_send = reddit_send()
+    for comment in r_read.subreddit(target_sub).stream.comments():
+        if comment_level(comment):
+            subs = await auth_report(comment.author)
+            try:
+                reply = False
+                day_old = subs[target_sub].posts_older_than('day')
+                week_old = subs[target_sub].posts_older_than('week')
+                if len(subs[target_sub]) == 1:
+                    msg = msg_reply('new')
+                    reply = True
+                elif (day_old == 0):
+                    msg = msg_reply('day')
+                    reply = True
+                elif (week_old == 0):
+                    msg = msg_reply('week')
+                    reply = True
+                if reply:
+                    send_comment = r_send.comment(id=comment.id)
+                    send_comment.reply(msg)
+            except:
+                print(comment.permalink)
+
+async def get_refresh():
+    r = reddit_connect()
+#    print(r.auth.scopes())
+#    state = str(random.randint(0, 65000))
+#    url = r.auth.url(["*"], state, "permanent")
+#    print(url)
+#    print(r.auth.authorize('0avQEiNqrNaX2Pi7wb-t7KcAD14'))
+
 
 
 if __name__ == "__main__":
